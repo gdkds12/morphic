044a3df63676df8eb68550c18d193402c94a2a4e
gdkds12 <gdkds12@gmail.com>
2024-08-16 00:41:03 +0900

    Update followup-panel.tsx

--- components/followup-panel.tsx
+++ components/followup-panel.tsx
@@ -42,7 +42,7 @@ export function FollowupPanel() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="fixed bottom-0 left-0 right-0 flex items-center space-x-1 bg-white p-2 shadow-md"
+      className="fixed relative flex items-center space-x-1"
     >
       <Input
         type="text"
