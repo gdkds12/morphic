adc72c2137cef6385ca2dd9bf6de52ce996c58a9
gdkds12 <gdkds12@gmail.com>
2024-08-17 03:10:00 +0900

    Update actions.tsx

--- app/actions.tsx
+++ app/actions.tsx
@@ -419,7 +419,7 @@ export const getUIStateFromAIState = (aiState: Chat) => {
               return {
                 id,
                 component: (
-                  <Section title="Follow-up" className="pb-8">
+                  <Section title="Follow-up" className="pb-1">
                     <FollowupPanel />
                   </Section>
                 )
