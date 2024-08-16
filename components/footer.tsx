2b2e0a27e67c29c8131710f26316e941003381ad
gdkds12 <gdkds12@gmail.com>
2024-08-17 02:41:38 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,42 +1,43 @@
 import React from 'react'
-import Link from 'next/link'
-import { SiDiscord, SiGithub, SiTwitter } from 'react-icons/si'
-import { Button } from './ui/button'
+// import Link from 'next/link'
+// import { SiDiscord, SiGithub, SiTwitter } from 'react-icons/si'
+// import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
-  return (
-    <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
-      <div className="flex justify-end">
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50 hidden"
-        >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50 hidden"
-        >
-          <Link href="https://twitter.com/morphic_ai" target="_blank">
-            <SiTwitter size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50 hidden"
-        >
-          <Link href="https://git.new/morphic" target="_blank">
-            <SiGithub size={18} />
-          </Link>
-        </Button>
-      </div>
-    </footer>
-  )
+  // return (
+  //   <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
+  //     <div className="flex justify-end">
+  //       <Button
+  //         variant={'ghost'}
+  //         size={'icon'}
+  //         className="text-muted-foreground/50 hidden"
+  //       >
+  //         <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
+  //           <SiDiscord size={18} />
+  //         </Link>
+  //       </Button>
+  //       <Button
+  //         variant={'ghost'}
+  //         size={'icon'}
+  //         className="text-muted-foreground/50 hidden"
+  //       >
+  //         <Link href="https://twitter.com/morphic_ai" target="_blank">
+  //           <SiTwitter size={18} />
+  //         </Link>
+  //       </Button>
+  //       <Button
+  //         variant={'ghost'}
+  //         size={'icon'}
+  //         className="text-muted-foreground/50 hidden"
+  //       >
+  //         <Link href="https://git.new/morphic" target="_blank">
+  //           <SiGithub size={18} />
+  //         </Link>
+  //       </Button>
+  //     </div>
+  //   </footer>
+  // )
+  return null
 }
 
 export default Footer
