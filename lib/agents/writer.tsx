74151c87ee4de8492203558bc4868f4ac16eef22
gdkds12 <gdkds12@gmail.com>
2024-08-17 04:21:48 +0900

    개선

--- lib/agents/writer.tsx
+++ lib/agents/writer.tsx
@@ -23,11 +23,10 @@ export async function writer(
   await streamText({
     model: openai!.chat(process.env.SPECIFIC_API_MODEL || 'llama3-70b-8192'),
     maxTokens: 2500,
-    system: `As a professional writer, your job is to generate a comprehensive and informative, yet concise answer of 600 words or less for the given question based solely on the provided search results (URL and content). You must only use information from the provided search results. Use an unbiased and journalistic tone. Combine search results together into a coherent answer. Do not repeat text. If there are any images relevant to your answer, be sure to include them as well. Aim to directly address the user's question, augmenting your response with insights gleaned from the search results. 
-    Whenever quoting or referencing information from a specific URL, always cite the source URL explicitly. Please match the language of the response to the user's language.
-    Always answer in Markdown format. Links and images must follow the correct format.
-    Link format: [link text](url)
-    Image format: ![alt text](url)
+    system: `당신은 전문 작가입니다, 당신의 임무는 주어진 질문에 대해 제공된 검색 결과(URL and content)를 바탕으로 답변을 작성하는 것입니다. 편견 없고 저널리즘적인 어조를 유지하며, 
+    검색 결과를 인용하고, 출처를 제공해야 합니다. 답변은 최소 3문단 이상이어야 하며, 코드를 제외한 글이 1000자를 넘지 않아야 합니다. 텍스트를 반복하지 말고, 답변에 관련된 이미지가 있는경우
+    반드시 포함시켜주세요. 특정 URL에서 인용하거나 참조할 때는 항상 출처 URL을 명시해주세요. 응답은 항상 한국어로 작성되어야 합니다. 항상 Markdown 형식을 사용해주세요. 링크와
+    이미지는 올바른 형식을 따라야합니다. Link format: [link text](url) Image format: ![alt text](url)
     `,
     messages,
     onFinish: event => {
